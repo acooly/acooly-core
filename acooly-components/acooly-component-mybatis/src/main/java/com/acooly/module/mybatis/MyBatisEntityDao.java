55bb9a57ef1135c476c6a9836e4f683a8381bb7f
邱占波 <bohr.qiu@gmail.com>
2017-01-07 15:49:00 +0800

    增加父接口，提供更多能力

--- acooly-components/acooly-component-mybatis/src/main/java/com/acooly/module/mybatis/MyBatisEntityDao.java
+++ acooly-components/acooly-component-mybatis/src/main/java/com/acooly/module/mybatis/MyBatisEntityDao.java
@@ -11,11 +11,12 @@ package com.acooly.module.mybatis;
 
 import com.acooly.core.common.dao.EntityDao;
 import com.acooly.module.mybatis.ex.*;
+import tk.mybatis.mapper.common.Mapper;
 
 /**
  * @author qiubo@yiji.com
  */
-public interface MyBatisEntityDao<T>	extends EntityDao<T>, CreateMapper<T>, GetMapper<T>, UpdateMapper<T>,
+public interface MyBatisEntityDao<T>	extends EntityDao<T>, Mapper<T>, CreateMapper<T>, GetMapper<T>, UpdateMapper<T>,
 									FlushMapper<T>, RemoveMapper<T>, SavesMapper<T>, FindMapper<T>, ListMapper<T> {
 	
 }

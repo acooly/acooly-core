05180cb0719dafc8a7e7ef2e1f9354159e77e000
张浦 <kuli@yiji.com>
2017-01-15 18:27:25 +0800

    动态添加时,如果条件值为空或空字符串时,忽悠查询条件~

--- acooly-core/src/main/java/com/acooly/core/common/dao/support/SearchFilter.java
+++ acooly-core/src/main/java/com/acooly/core/common/dao/support/SearchFilter.java
@@ -1,5 +1,6 @@
 package com.acooly.core.common.dao.support;
 
+import com.acooly.core.utils.Strings;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.Assert;
@@ -10,44 +11,46 @@ import java.util.Map.Entry;
 
 /**
  * 动态查询 条件
- * 
+ *
  * @author zhangpu
  * @date 2012年6月30日
  */
 public class SearchFilter {
-	
-	public enum Operator {
-		EQ, NEQ, LIKE, LLIKE, RLIKE, GT, LT, GTE, LTE, IN, NOTIN, NULL, NOTNULL,
-	}
-	
-	public String fieldName;
-	public Object value;
-	public Operator operator;
-	
-	public SearchFilter(String fieldName, Operator operator, Object value) {
-		this.fieldName = fieldName;
-		this.value = value;
-		this.operator = operator;
-	}
-	
-	public static List<SearchFilter> parse(Map<String, Object> searchParams) {
-		List<SearchFilter> filters = Lists.newArrayList();
-		for (Entry<String, Object> entry : searchParams.entrySet()) {
-			SearchFilter searchFilter = parse(entry.getKey(), entry.getValue());
-			if (searchFilter != null) {
-				filters.add(searchFilter);
-			}
-		}
-		return filters;
-	}
-	
-	public static SearchFilter parse(String param, Object value) {
-		Assert.hasText(param);
-		String[] names = StringUtils.split(param, "_");
-		if (names.length != 2) {
-			throw new IllegalArgumentException(param + " is not a valid search filter name");
-		}
-		return new SearchFilter(names[1], Operator.valueOf(names[0].toUpperCase()), value);
-		
-	}
+
+    public enum Operator {
+        EQ, NEQ, LIKE, LLIKE, RLIKE, GT, LT, GTE, LTE, IN, NOTIN, NULL, NOTNULL,
+    }
+
+    public String fieldName;
+    public Object value;
+    public Operator operator;
+
+    public SearchFilter(String fieldName, Operator operator, Object value) {
+        this.fieldName = fieldName;
+        this.value = value;
+        this.operator = operator;
+    }
+
+    public static List<SearchFilter> parse(Map<String, Object> searchParams) {
+        List<SearchFilter> filters = Lists.newArrayList();
+        for (Entry<String, Object> entry : searchParams.entrySet()) {
+            SearchFilter searchFilter = parse(entry.getKey(), entry.getValue());
+            if (searchFilter != null) {
+                filters.add(searchFilter);
+            }
+        }
+        return filters;
+    }
+
+    public static SearchFilter parse(String param, Object value) {
+        Assert.hasText(param);
+        if (value == null || Strings.isBlank(value.toString())) {
+            return null;
+        }
+        String[] names = StringUtils.split(param, "_");
+        if (names.length != 2) {
+            throw new IllegalArgumentException(param + " is not a valid search filter name");
+        }
+        return new SearchFilter(names[1], Operator.valueOf(names[0].toUpperCase()), value);
+    }
 }

5773ed7510ee972f3a4f1902fc96cd36f5e1fe41
邱占波 <bohr.qiu@gmail.com>
2017-03-02 17:28:14 +0800

    修改mybatis money typehandler

--- acooly-components/acooly-component-mybatis/src/main/java/com/acooly/module/mybatis/typehandler/MoneyTypeHandler.java
+++ acooly-components/acooly-component-mybatis/src/main/java/com/acooly/module/mybatis/typehandler/MoneyTypeHandler.java
@@ -3,6 +3,7 @@ package com.acooly.module.mybatis.typehandler;
 import com.acooly.core.utils.Money;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
 import org.apache.ibatis.type.MappedTypes;
 
 import java.math.BigDecimal;
@@ -20,48 +21,37 @@ import java.sql.SQLException;
  *
  */
 @MappedTypes(Money.class)
+@MappedJdbcTypes({ JdbcType.BIGINT, JdbcType.DECIMAL, JdbcType.NUMERIC })
 public class MoneyTypeHandler extends BaseTypeHandler<Money> {
 
-	public MoneyTypeHandler(Class<Money> type) {
-		if (type == null) {
-			throw new IllegalArgumentException("Type argument cannot be null");
-		}
-	}
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Money parameter,
+                                    JdbcType jdbcType) throws SQLException {
+        ps.setLong(i, parameter.getCent());
+    }
 
-	@Override
-	public void setNonNullParameter(PreparedStatement ps, int i, Money parameter, JdbcType jdbcType)
-	        throws SQLException {
-		ps.setBigDecimal(i, parameter.getAmount());
-	}
+    @Override
+    public Money getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        long result = rs.getLong(columnName);
+        Money money = new Money();
+        money.setCent(result);
+        return money;
+    }
 
-	@Override
-	public Money getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-		BigDecimal bd = cs.getBigDecimal(columnIndex);
-		if (cs.wasNull()) {
-			return null;
-		} else {
-			return new Money(bd);
-		}
-	}
+    @Override
+    public Money getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        long result = rs.getLong(columnIndex);
+        Money money = new Money();
+        money.setCent(result);
+        return money;
+    }
 
-	@Override
-	public Money getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-		BigDecimal bd = rs.getBigDecimal(columnIndex);
-		if (rs.wasNull()) {
-			return null;
-		} else {
-			return new Money(bd);
-		}
-	}
-
-	@Override
-	public Money getNullableResult(ResultSet rs, String columnName) throws SQLException {
-		BigDecimal bd = rs.getBigDecimal(columnName);
-		if (rs.wasNull()) {
-			return null;
-		} else {
-			return new Money(bd);
-		}
-	}
+    @Override
+    public Money getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        long result = cs.getLong(columnIndex);
+        Money money = new Money();
+        money.setCent(result);
+        return money;
+    }
 
 }

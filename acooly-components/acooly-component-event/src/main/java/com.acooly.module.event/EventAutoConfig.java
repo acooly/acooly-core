8e341276bfb78b4597e76219a21158381bed38d0
邱占波 <bohr.qiu@gmail.com>
2017-03-10 16:25:09 +0800

    增加事件处理器

---
+++ acooly-components/acooly-component-event/src/main/java/com.acooly.module.event/EventAutoConfig.java
@@ -0,0 +1,69 @@
+package com.acooly.module.event;
+
+import com.acooly.core.common.boot.Apps;
+import lombok.extern.slf4j.Slf4j;
+import net.engio.mbassy.bus.config.BusConfiguration;
+import net.engio.mbassy.bus.config.Feature;
+import net.engio.mbassy.bus.config.IBusConfiguration;
+import net.engio.mbassy.bus.error.IPublicationErrorHandler;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableConfigurationProperties({ EventProperties.class })
+@Slf4j
+public class EventAutoConfig {
+	private Map<String, Object> beansWithAnnotation;
+	
+	@Bean
+	public EventBus messageBus(ThreadPoolTaskExecutor asyncEventExecutorService) {
+		Feature.AsynchronousHandlerInvocation asynchronousHandlerInvocation = new Feature.AsynchronousHandlerInvocation();
+		asynchronousHandlerInvocation.setExecutor(asyncEventExecutorService.getThreadPoolExecutor());
+		EventBus bus = new EventBus(new BusConfiguration().addFeature(Feature.SyncPubSub.Default())
+			.addFeature(asynchronousHandlerInvocation).addFeature(Feature.AsynchronousMessageDispatch.Default())
+			.addPublicationErrorHandler(new IPublicationErrorHandler.ConsoleLogger())
+			.setProperty(IBusConfiguration.Properties.BusId, "global bus"));
+		return bus;
+	}
+	
+	@Bean
+	public ThreadPoolTaskExecutor asyncEventExecutorService(EventProperties properties) {
+		ThreadPoolTaskExecutor bean = new ThreadPoolTaskExecutor();
+		bean.setCorePoolSize(properties.getThreadMin());
+		bean.setMaxPoolSize(properties.getThreadMax());
+		bean.setQueueCapacity(properties.getThreadQueue());
+		bean.setKeepAliveSeconds(300);
+		bean.setThreadNamePrefix("async-event-");
+		bean.setWaitForTasksToCompleteOnShutdown(true);
+		bean.setAllowCoreThreadTimeOut(true);
+		bean.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		return bean;
+	}
+	
+	@PostConstruct
+	public void subscribeEventHandler() {
+		beansWithAnnotation = Apps.getApplicationContext().getBeansWithAnnotation(EventHandler.class);
+		EventBus eventBus = Apps.getApplicationContext().getBean(EventBus.class);
+		for (Object o : beansWithAnnotation.values()) {
+			eventBus.subscribe(o);
+			log.info("注册事件处理器:", o.getClass().getName());
+		}
+	}
+	
+	@PreDestroy
+	public void registerEventHandler() {
+		beansWithAnnotation = Apps.getApplicationContext().getBeansWithAnnotation(EventHandler.class);
+		EventBus eventBus = Apps.getApplicationContext().getBean(EventBus.class);
+		for (Object o : beansWithAnnotation.values()) {
+			eventBus.unsubscribe(o);
+			log.info("销毁事件处理器:", o.getClass().getName());
+		}
+	}
+}
---
+++ acooly-core-test/src/test/java/com/acooly/core/test/event/CustomerEventHandler.java
@@ -0,0 +1,35 @@
+/*
+ * www.yiji.com Inc.
+ * Copyright (c) 2017 All Rights Reserved
+ */
+
+/*
+ * 修订记录:
+ * qiubo@yiji.com 2017-03-10 16:11 创建
+ */
+package com.acooly.core.test.event;
+
+import com.acooly.module.event.EventHandler;
+import lombok.extern.slf4j.Slf4j;
+import net.engio.mbassy.listener.Handler;
+import net.engio.mbassy.listener.Invoke;
+
+/**
+ * @author qiubo@yiji.com
+ */
+@EventHandler
+@Slf4j
+public class CustomerEventHandler {
+    //同步事件处理器
+    @Handler
+    public void handleCreateCustomerEvent(CreateCustomerEvent event) {
+        log.info("{}",event);
+        //do what you like
+    }
+    //异步事件处理器
+    @Handler(delivery = Invoke.Asynchronously)
+    public void handleCreateCustomerEventAsyn(CreateCustomerEvent event) {
+        log.info("{}",event);
+        //do what you like
+    }
+}

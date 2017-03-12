fd41473c93051d81151b74ea2f6d0e26b25514bb
张浦 <kuli@yiji.com>
2017-03-12 16:41:11 +0800

    删除SimpleStatus枚举的toString复写，保持原有默认实现，以方便与数据库的值匹配，解决前端BOSS的编辑界面回绑下拉类别问题

--- acooly-core/src/main/java/com/acooly/core/utils/enums/SimpleStatus.java
+++ acooly-core/src/main/java/com/acooly/core/utils/enums/SimpleStatus.java
@@ -7,103 +7,97 @@
  */
 package com.acooly.core.utils.enums;
 
+import com.google.common.collect.Maps;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.collect.Maps;
-
 /**
  * 简单状态
- * 
+ *
  * @author zhangpu
  */
 public enum SimpleStatus implements Messageable {
 
-	enable("enable", "正常"),
+    enable("enable", "正常"),
 
-	pause("pause", "暂停"),
+    pause("pause", "暂停"),
 
-	disable("disable", "禁用");
+    disable("disable", "禁用");
 
-	private final String code;
-	private final String message;
+    private final String code;
+    private final String message;
 
-	private SimpleStatus(String code, String message) {
-		this.code = code;
-		this.message = message;
-	}
+    private SimpleStatus(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public String code() {
-		return code;
-	}
+    public String code() {
+        return code;
+    }
 
-	public String message() {
-		return message;
-	}
+    public String message() {
+        return message;
+    }
 
-	public static Map<String, String> mapping() {
-		Map<String, String> map = Maps.newLinkedHashMap();
-		for (SimpleStatus type : values()) {
-			map.put(type.getCode(), type.getMessage());
-		}
-		return map;
-	}
+    public static Map<String, String> mapping() {
+        Map<String, String> map = Maps.newLinkedHashMap();
+        for (SimpleStatus type : values()) {
+            map.put(type.getCode(), type.getMessage());
+        }
+        return map;
+    }
 
-	/**
-	 * 通过枚举值码查找枚举值。
-	 * 
-	 * @param code
-	 *            查找枚举值的枚举值码。
-	 * @return 枚举值码对应的枚举值。
-	 * @throws IllegalArgumentException
-	 *             如果 code 没有对应的 Status 。
-	 */
-	public static SimpleStatus findStatus(String code) {
-		for (SimpleStatus status : values()) {
-			if (status.getCode().equals(code)) {
-				return status;
-			}
-		}
-		throw new IllegalArgumentException("SimpleStatus not legal:" + code);
-	}
+    /**
+     * 通过枚举值码查找枚举值。
+     *
+     * @param code 查找枚举值的枚举值码。
+     * @return 枚举值码对应的枚举值。
+     * @throws IllegalArgumentException 如果 code 没有对应的 Status 。
+     */
+    public static SimpleStatus findStatus(String code) {
+        for (SimpleStatus status : values()) {
+            if (status.getCode().equals(code)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("SimpleStatus not legal:" + code);
+    }
 
-	/**
-	 * 获取全部枚举值。
-	 * 
-	 * @return 全部枚举值。
-	 */
-	public static List<SimpleStatus> getAllStatus() {
-		List<SimpleStatus> list = new ArrayList<SimpleStatus>();
-		for (SimpleStatus status : values()) {
-			list.add(status);
-		}
-		return list;
-	}
+    /**
+     * 获取全部枚举值。
+     *
+     * @return 全部枚举值。
+     */
+    public static List<SimpleStatus> getAllStatus() {
+        List<SimpleStatus> list = new ArrayList<SimpleStatus>();
+        for (SimpleStatus status : values()) {
+            list.add(status);
+        }
+        return list;
+    }
 
-	/**
-	 * 获取全部枚举值码。
-	 * 
-	 * @return 全部枚举值码。
-	 */
-	public static List<String> getAllStatusCode() {
-		List<String> list = new ArrayList<String>();
-		for (SimpleStatus status : values()) {
-			list.add(status.code());
-		}
-		return list;
-	}
+    /**
+     * 获取全部枚举值码。
+     *
+     * @return 全部枚举值码。
+     */
+    public static List<String> getAllStatusCode() {
+        List<String> list = new ArrayList<String>();
+        for (SimpleStatus status : values()) {
+            list.add(status.code());
+        }
+        return list;
+    }
 
-	@Override
-	public String toString() {
-		return String.format("%s:%s", this.code, this.message);
-	}
 }

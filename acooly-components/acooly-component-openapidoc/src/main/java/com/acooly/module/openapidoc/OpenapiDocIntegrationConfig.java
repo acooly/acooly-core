d34d2a724ddb6c89928b1600c4dfaeb06212e998
邱占波 <bohr.qiu@gmail.com>
2017-04-05 09:58:02 +0800

    修复acooly.openapidoc.enable使用时异常

--- acooly-components/acooly-component-openapidoc/src/main/java/com/acooly/module/openapidoc/OpenapiDocIntegrationConfig.java
+++ acooly-components/acooly-component-openapidoc/src/main/java/com/acooly/module/openapidoc/OpenapiDocIntegrationConfig.java
@@ -12,6 +12,7 @@ package com.acooly.module.openapidoc;
 import com.acooly.module.jpa.ex.AbstractEntityJpaDao;
 import com.acooly.module.security.config.SecurityAutoConfig;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -20,6 +21,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @author qiubo@yiji.com
  */
 @Configuration
+@ConditionalOnProperty(value = "acooly.openapidoc.enable", matchIfMissing = true)
 @ComponentScan(basePackages = "com.yiji.framework.openapidoc")
 @EnableJpaRepositories(repositoryBaseClass = AbstractEntityJpaDao.class, basePackages = "com.yiji.framework.openapidoc")
 @AutoConfigureAfter(SecurityAutoConfig.class)

8713d63fdce78ea1bf8c7140dc72c3fd7bcff111
邱占波 <bohr.qiu@gmail.com>
2017-04-13 01:00:47 +0800

    add test

--- acooly-core-test/src/main/java/com/acooly/core/test/web/TestController.java
+++ acooly-core-test/src/main/java/com/acooly/core/test/web/TestController.java
@@ -15,6 +15,7 @@ import com.acooly.module.mail.MailService;
 import com.acooly.module.sms.SmsService;
 import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,8 @@ public class TestController {
 	private SmsService smsService;
 	@Autowired
 	private MailService mailService;
+    @Value("${prop}")
+    private String valueFromProp;
 	
 	@RequestMapping("sms")
 	public void testSms() {

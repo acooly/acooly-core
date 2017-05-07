15056fe78a8b0aff416a4a416697480c2d1541c1
邱占波 <bohr.qiu@gmail.com>
2017-05-07 13:40:48 +0800

    add install script

--- scripts/init/common.sh
+++ scripts/init/common.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-yum install lrzsz -y
-yum install git -y
-yum install telnet -y
+# use aliyun yum mirror
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+yum clean all
+yum makecache
+
+yum install lrzsz git telnet -y

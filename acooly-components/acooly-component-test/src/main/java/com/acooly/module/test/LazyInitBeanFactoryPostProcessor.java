8d0bb8b65231a789d5849a797a43d66c3c091c94
邱占波 <bohr.qiu@gmail.com>
2017-07-06 20:23:39 +0800

    refactor

--- acooly-components/acooly-component-test/src/main/java/com/acooly/module/test/LazyInitBeanFactoryPostProcessor.java
+++ acooly-components/acooly-component-test/src/main/java/com/acooly/module/test/LazyInitBeanFactoryPostProcessor.java
@@ -1,45 +1,58 @@
 package com.acooly.module.test;
 
+import com.acooly.core.common.boot.Apps;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
 
-/**
- * @author qiubo@yiji.com
- */
+/** @author qiubo@yiji.com */
 public class LazyInitBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
-    private Class<?>[] exclusionList;
+  private Class<?>[] exclusionList;
 
-    public LazyInitBeanFactoryPostProcessor() {
+  private Boolean isTest;
+
+  public LazyInitBeanFactoryPostProcessor() {}
+
+  public LazyInitBeanFactoryPostProcessor(Class<?>[] exclusionList) {
+    this.exclusionList = exclusionList;
+  }
+
+  @Override
+  public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+      throws BeansException {
+    if (isTest == null) {
+      ConfigurableEnvironment environment = (ConfigurableEnvironment) Apps.getEnvironment();
+      MutablePropertySources propertySources = environment.getPropertySources();
+      isTest = propertySources.contains("Inlined Test Properties");
     }
-
-    public LazyInitBeanFactoryPostProcessor(Class<?>[] exclusionList) {
-        this.exclusionList = exclusionList;
+    if (!isTest) {
+      return;
     }
-
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-
-        //Iterate over all bean, mark them as lazy if they are not in the exclusion list.
-        for (String beanName : beanFactory.getBeanDefinitionNames()) {
-            if (isLazy(beanName, beanFactory)) {
-                BeanDefinition definition = beanFactory.getBeanDefinition(beanName);
-                definition.setLazyInit(true);
-            }
+    for (String beanName : beanFactory.getBeanDefinitionNames()) {
+      if (isLazy(beanName, beanFactory)) {
+        BeanDefinition definition = beanFactory.getBeanDefinition(beanName);
+        String beanClassName = definition.getBeanClassName();
+        if (beanClassName != null && beanClassName.contains("com.acooly.module.mybatis")) {
+          continue;
         }
+        definition.setLazyInit(true);
+      }
     }
+  }
 
-    private boolean isLazy(String beanName, ConfigurableListableBeanFactory beanFactory) {
-        if (exclusionList == null || exclusionList.length == 0) {
-            return true;
-        }
-        for (Class<?> clazz : exclusionList) {
-            if (beanFactory.isTypeMatch(beanName,clazz)) {
-                return false;
-            }
-        }
-        return true;
+  private boolean isLazy(String beanName, ConfigurableListableBeanFactory beanFactory) {
+    if (exclusionList == null || exclusionList.length == 0) {
+      return true;
     }
+    for (Class<?> clazz : exclusionList) {
+      if (beanFactory.isTypeMatch(beanName, clazz)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }

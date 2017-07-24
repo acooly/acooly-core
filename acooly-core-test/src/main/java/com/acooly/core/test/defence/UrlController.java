a3acdc4d837d00ad495f3757031140d87803e7c0
邱占波 <bohr.qiu@gmail.com>
2017-07-24 17:47:58 +0800

    增加url加密组件

--- acooly-core-test/src/main/java/com/acooly/core/test/defence/UrlController.java
+++ acooly-core-test/src/main/java/com/acooly/core/test/defence/UrlController.java
@@ -4,9 +4,13 @@ import com.acooly.module.defence.DefenceProperties;
 import com.acooly.module.defence.url.SecurityParam;
 import com.acooly.module.defence.url.UrlSecurityServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /** @author qiubo@yiji.com */
 @RestController
 @RequestMapping("/url")
@@ -22,7 +26,7 @@ public class UrlController {
   /**
    * 支持类型转换
    *
-   * 请求：http://127.0.0.1:8081/url/param1?id=58bb0e40e63678e780590e5986cbd804be7b5a11379de9f4f6a6238287bd5779
+   * <p>请求：http://127.0.0.1:8081/url/param1?id=58bb0e40e63678e780590e5986cbd804be7b5a11379de9f4f6a6238287bd5779
    */
   @RequestMapping(value = "/param1", method = RequestMethod.GET)
   public Integer param1(@SecurityParam Integer id) {
@@ -31,7 +35,7 @@ public class UrlController {
   /**
    * 支持POJO
    *
-   * 请求：http://127.0.0.1:8081/url/param2?id=58bb0e40e63678e780590e5986cbd804be7b5a11379de9f4f6a6238287bd5779&name=bohr&age=12
+   * <p>请求：http://127.0.0.1:8081/url/param2?id=58bb0e40e63678e780590e5986cbd804be7b5a11379de9f4f6a6238287bd5779&name=bohr&age=12
    */
   @RequestMapping(value = "/param2", method = RequestMethod.GET)
   public MyDto param2(@SecurityParam({"id"}) MyDto app) {
@@ -41,14 +45,30 @@ public class UrlController {
   /**
    * 支持响应加密
    *
-   * 请求：http://127.0.0.1:8081/url/param3?id=58bb0e40e63678e780590e5986cbd804be7b5a11379de9f4f6a6238287bd5779&name=bohr&age=12
+   * <p>请求：http://127.0.0.1:8081/url/param3?id=58bb0e40e63678e780590e5986cbd804be7b5a11379de9f4f6a6238287bd5779&name=bohr&age=12
    * 响应：{"id":"58bb0e40e63678e780590e5986cbd804be7b5a11379de9f4f6a6238287bd5779","name":"bohr","age":1}
    */
   @RequestMapping(value = "/param3", method = RequestMethod.GET)
-  public @SecurityParam({"id"}) MyDto param4(@SecurityParam({"id"}) MyDto app) {
+  public @SecurityParam({"id"}) MyDto param3(@SecurityParam({"id"}) MyDto app) {
     log.info("{}", app);
     app.setAge(1);
     return app;
+  }
+
+  @RequestMapping(value = "/param4", method = RequestMethod.GET)
+  public @SecurityParam({"id"}) List<MyDto> param4() {
+    List<MyDto> dtos = Lists.newArrayList();
+    MyDto myDto = new MyDto();
+    myDto.setId(1l);
+    myDto.setName("bohr");
+    myDto.setAge(30);
+    MyDto myDto1 = new MyDto();
+    myDto1.setId(2l);
+    myDto1.setName("na");
+    myDto1.setAge(28);
+    dtos.add(myDto);
+    dtos.add(myDto1);
+    return dtos;
   }
 
   public static void main(String[] args) throws Exception {

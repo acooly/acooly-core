4dc7956febd44413355ebafb865f3132fabd8648
邱占波 <bohr.qiu@gmail.com>
2017-08-11 19:14:59 +0800

    add order

--- acooly-common/src/main/java/com/acooly/core/common/facade/BizOrderBase.java
+++ acooly-common/src/main/java/com/acooly/core/common/facade/BizOrderBase.java
@@ -1,11 +1,15 @@
 package com.acooly.core.common.facade;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * 命令请求基类
  * @author qiubo@yiji.com
  */
+@Getter
+@Setter
 public class BizOrderBase extends OrderBase {
   /** 商户订单号 */
   private String merchOrderNo;

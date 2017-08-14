3a7745b5278d01447d96c1f2af675c8a19d74a8e
邱占波 <bohr.qiu@gmail.com>
2017-08-14 17:56:15 +0800

    m

--- acooly-components/acooly-component-jpa/src/main/java/com/acooly/module/jpa/ExHibernateJpaVendorAdapter.java
+++ acooly-components/acooly-component-jpa/src/main/java/com/acooly/module/jpa/ExHibernateJpaVendorAdapter.java
@@ -1,7 +1,6 @@
 package com.acooly.module.jpa;
 
 import com.acooly.core.common.boot.ApplicationContextHolder;
-import com.acooly.core.common.exception.AppConfigException;
 import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
@@ -64,30 +63,30 @@ public class ExHibernateJpaVendorAdapter extends HibernateJpaVendorAdapter {
   private void exeScripts(Connection connection, String dbType, List<String> scripts) {
     scripts.forEach(
         s -> {
+          Resource[] scriptResources;
           try {
-            Resource[] scriptResources = ApplicationContextHolder.get().getResources(s);
-            Arrays.stream(scriptResources)
-                .filter(Resource::exists).sorted()
-                .forEach(
-                    resource -> {
-                      log.info(
-                          "执行自定义数据库脚本文件:META-INF/database/{}/{}", dbType, resource.getFilename());
-                      EncodedResource encodedResource =
-                          new EncodedResource(resource, Charsets.UTF_8);
-                      ScriptUtils.executeSqlScript(
-                          connection,
-                          encodedResource,
-                          true,
-                          true,
-                          DEFAULT_COMMENT_PREFIX,
-                          DEFAULT_STATEMENT_SEPARATOR,
-                          DEFAULT_BLOCK_COMMENT_START_DELIMITER,
-                          DEFAULT_BLOCK_COMMENT_END_DELIMITER);
-                    });
-
+            scriptResources = ApplicationContextHolder.get().getResources(s);
           } catch (IOException e) {
-            throw new AppConfigException(e);
+            return;
           }
+          Arrays.stream(scriptResources)
+              .filter(Resource::exists)
+              .sorted()
+              .forEach(
+                  resource -> {
+                    log.info(
+                        "执行自定义数据库脚本文件:META-INF/database/{}/{}", dbType, resource.getFilename());
+                    EncodedResource encodedResource = new EncodedResource(resource, Charsets.UTF_8);
+                    ScriptUtils.executeSqlScript(
+                        connection,
+                        encodedResource,
+                        true,
+                        true,
+                        DEFAULT_COMMENT_PREFIX,
+                        DEFAULT_STATEMENT_SEPARATOR,
+                        DEFAULT_BLOCK_COMMENT_START_DELIMITER,
+                        DEFAULT_BLOCK_COMMENT_END_DELIMITER);
+                  });
         });
   }
 }

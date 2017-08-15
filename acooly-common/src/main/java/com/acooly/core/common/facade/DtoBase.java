300fe42d1351b4104b9f6f2fdd4d9f3f50c15b85
邱占波 <bohr.qiu@gmail.com>
2017-08-15 16:04:46 +0800

    增加方法

--- acooly-common/src/main/java/com/acooly/core/common/facade/DtoBase.java
+++ acooly-common/src/main/java/com/acooly/core/common/facade/DtoBase.java
@@ -9,29 +9,17 @@
  */
 package com.acooly.core.common.facade;
 
-import com.acooly.core.common.exception.OrderCheckException;
-import com.acooly.core.utils.ToString;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
 
-import java.io.Serializable;
-
 /** @author qiubo@yiji.com */
 @Getter
 @Setter
-public class DtoBase implements Serializable {
+public class DtoBase extends InfoBase {
   private static final long serialVersionUID = 1L;
   /** 商户订单号 */
   private String merchOrderNo;
   /** 业务订单号 * */
   @NotEmpty private String bizOrderNo;
-
-  @Override
-  public String toString() {
-    return ToString.toString(this);
-  }
-
-  /** 请求参数校验,在使用@AppService并且没有启用校验组@AppService.ValidationGroup时会被调用 */
-  public void check() throws OrderCheckException {}
 }

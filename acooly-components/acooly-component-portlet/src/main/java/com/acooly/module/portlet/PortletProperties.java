82a0737813536703b4cb93f0176c422682d2f35e
张浦 <kuli@yiji.com>
2017-08-18 00:37:22 +0800

    优化acooly-component-portlet组件的actionLog特性。解决url超过128字符时的溢出BUG（需要修改action_key列为128长度）；日志写入时，加入了缓存处理，批量写入。

--- acooly-components/acooly-component-portlet/src/main/java/com/acooly/module/portlet/PortletProperties.java
+++ acooly-components/acooly-component-portlet/src/main/java/com/acooly/module/portlet/PortletProperties.java
@@ -14,28 +14,51 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-/** @author qiubo */
+/**
+ * @author qiubo
+ */
 @ConfigurationProperties(PortletProperties.PREFIX)
 @Data
 public class PortletProperties {
 
-  public static final String PREFIX = "acooly.portlet";
-  /** 是否启用 */
-  private boolean enable = true;
-  /** 访问日志 */
-  private ActionLog actionlog = new ActionLog();
+    public static final String PREFIX = "acooly.portlet";
+    /**
+     * 是否启用
+     */
+    private boolean enable = true;
+    /**
+     * 访问日志
+     */
+    private ActionLog actionlog = new ActionLog();
 
-  /** 访问日志 */
-  @Getter
-  @Setter
-  public static class ActionLog {
+    /**
+     * 访问日志
+     */
+    @Getter
+    @Setter
+    public static class ActionLog {
 
-    /** 是否开启filter自动记录日志 */
-    private boolean filterEnable = false;
-    /** 需要拦截和记录的Url配置,支持AntPath,多个使用逗号分隔 */
-    private String filterUrlPatterns = "/portal/**";
+        /**
+         * 是否开启filter自动记录日志
+         */
+        private boolean filterEnable = false;
+        /**
+         * 需要拦截和记录的Url配置,支持AntPath,多个使用逗号分隔
+         */
+        private String filterUrlPatterns = "/portal/**";
 
-    /** 用户会话key,多个使用逗号分隔 */
-    private String sessionUserKey = "sessionCustomer";
-  }
+        /**
+         * 是否批量插入
+         */
+        private boolean cacheable = true;
+        /**
+         * 批量大小
+         */
+        private int cacheSize = 1000;
+
+        /**
+         * 用户会话key,多个使用逗号分隔
+         */
+        private String sessionUserKey = "sessionCustomer";
+    }
 }
--- acooly-components/acooly-component-portlet/src/main/java/com/acooly/module/portlet/service/impl/ActionLogServiceImpl.java
+++ acooly-components/acooly-component-portlet/src/main/java/com/acooly/module/portlet/service/impl/ActionLogServiceImpl.java
@@ -10,94 +10,147 @@ import com.acooly.core.common.service.EntityServiceImpl;
 import com.acooly.core.utils.Servlets;
 import com.acooly.core.utils.Strings;
 import com.acooly.core.utils.system.IPUtil;
+import com.acooly.module.portlet.PortletProperties;
 import com.acooly.module.portlet.dao.ActionLogDao;
 import com.acooly.module.portlet.entity.ActionLog;
 import com.acooly.module.portlet.entity.ActionMapping;
 import com.acooly.module.portlet.enums.ActionChannelEnum;
 import com.acooly.module.portlet.service.ActionLogService;
 import com.acooly.module.portlet.service.ActionMappingService;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * portlet_action_log Service实现
- *
+ * <p>
  * <p>Date: 2017-03-20 23:36:29
  *
  * @author acooly
  */
 @Service("actionLogService")
 public class ActionLogServiceImpl extends EntityServiceImpl<ActionLog, ActionLogDao>
-    implements ActionLogService {
+        implements ActionLogService {
 
-  private static final Logger logger = LoggerFactory.getLogger(ActionLogServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(ActionLogServiceImpl.class);
 
-  @Resource private ActionMappingService actionMappingService;
+    @Resource
+    private ActionMappingService actionMappingService;
 
-  @Override
-  public ActionLog logger(
-      String action,
-      String actionName,
-      String userName,
-      ActionChannelEnum actionChannel,
-      String version,
-      String comments,
-      HttpServletRequest request) {
-    try {
-      ActionLog actionLog = new ActionLog();
-      actionLog.setActionKey(action);
-      actionLog.setActionName(actionName);
-      if (actionChannel == null && request == null) {
-        throw new IllegalArgumentException("actionChannel和request不能同时为空");
-      }
-      if (actionChannel == null && request != null) {
-        actionLog.setChannel(parseChannelForReqeust(request));
-      } else {
-        actionLog.setChannel(actionChannel);
-      }
-      actionLog.setComments(comments);
-      actionLog.setUserName(userName);
-      actionLog.setChannelVersion(version);
-      actionLog.setUserIp(IPUtil.getIpAddr(request));
-      if (request != null) {
-        actionLog.setChannelInfo(
-            Strings.substring(Servlets.getHeaderValue(request, "User-Agent"), 0, 255));
-      }
-      save(actionLog);
-      return actionLog;
-    } catch (Exception e) {
-      logger.warn("保持action日志失败:{}", e.getMessage());
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private PortletProperties portletProperties;
+
+    private static final String ACTION_LOG_CACHE_NAME = "portlet.action.actionlogs";
+    private static final int ACTION_LOG_CACHE_SIZE_DEFAULT = 100;
+
+
+    @Override
+    public ActionLog logger(HttpServletRequest request, String userName) {
+        String actionKey = Servlets.getRequestPath(request);
+        String actionName = null;
+        ActionMapping actionMapping = actionMappingService.getActionMapping(actionKey);
+        if (actionMapping != null) {
+            actionName = actionMapping.getTitle();
+        }
+        return logger(
+                actionKey, actionName, userName, parseChannelForReqeust(request), null, null, request);
     }
-    return null;
-  }
 
-  @Override
-  public ActionLog logger(HttpServletRequest request, String userName) {
-    String actionKey = Servlets.getRequestPath(request);
-    String actionName = null;
-    ActionMapping actionMapping = actionMappingService.getActionMapping(actionKey);
-    if (actionMapping != null) {
-      actionName = actionMapping.getTitle();
-    }
-    return logger(
-        actionKey, actionName, userName, parseChannelForReqeust(request), null, null, request);
-  }
+    @Override
+    public ActionLog logger(
+            String action,
+            String actionName,
+            String userName,
+            ActionChannelEnum actionChannel,
+            String version,
+            String comments,
+            HttpServletRequest request) {
 
-  protected ActionChannelEnum parseChannelForReqeust(HttpServletRequest request) {
-    String userAgent = Servlets.getHeaderValue(request, "User-Agent");
-    if (Strings.contains(userAgent, "MicroMessenger")) {
-      return ActionChannelEnum.wechat;
+        try {
+            ActionLog actionLog = doMarshallActionLog(action, actionName, userName, actionChannel, version, comments, request);
+            if (portletProperties.getActionlog().isCacheable()) {
+                int cacheSize = portletProperties.getActionlog().getCacheSize();
+                if (cacheSize <= 0) {
+                    cacheSize = ACTION_LOG_CACHE_SIZE_DEFAULT;
+                }
+
+                ValueOperations<String, List<ActionLog>> va = redisTemplate.opsForValue();
+
+                List<ActionLog> actionLogs = va.get(ACTION_LOG_CACHE_NAME);
+                if (actionLogs == null) {
+                    actionLogs = Lists.newArrayList();
+                }
+                actionLogs.add(actionLog);
+                if (actionLogs.size() >= cacheSize) {
+                    saves(actionLogs);
+                    redisTemplate.delete(ACTION_LOG_CACHE_NAME);
+                } else {
+                    va.set(ACTION_LOG_CACHE_NAME, actionLogs);
+                }
+
+            } else {
+                save(actionLog);
+            }
+            return actionLog;
+        } catch (Exception e) {
+            logger.warn("保持action日志失败:{}", e.getMessage());
+        }
+        return null;
     }
-    String[] iosMobiles = {"iPod", "iPad", "iPhone", "Android", "SymbianOS", "Windows Phone"};
-    for (String s : iosMobiles) {
-      if (Strings.containsIgnoreCase(userAgent, s)) {
-        return ActionChannelEnum.wap;
-      }
+
+    protected ActionLog doMarshallActionLog(String action,
+                                            String actionName,
+                                            String userName,
+                                            ActionChannelEnum actionChannel,
+                                            String version,
+                                            String comments,
+                                            HttpServletRequest request) {
+        ActionLog actionLog = new ActionLog();
+        actionLog.setActionKey(Strings.right(action, 128));
+        actionLog.setActionName(actionName);
+        if (actionChannel == null && request == null) {
+            throw new IllegalArgumentException("actionChannel和request不能同时为空");
+        }
+        if (actionChannel == null && request != null) {
+            actionLog.setChannel(parseChannelForReqeust(request));
+        } else {
+            actionLog.setChannel(actionChannel);
+        }
+        actionLog.setActionUrl(Strings.right(Servlets.getRequestPath(request), 128));
+        actionLog.setComments(comments);
+        actionLog.setUserName(userName);
+        actionLog.setChannelVersion(version);
+        actionLog.setUserIp(IPUtil.getIpAddr(request));
+        if (request != null) {
+            actionLog.setChannelInfo(
+                    Strings.substring(Servlets.getHeaderValue(request, "User-Agent"), 0, 255));
+        }
+        return actionLog;
     }
-    return ActionChannelEnum.web;
-  }
+
+
+    protected ActionChannelEnum parseChannelForReqeust(HttpServletRequest request) {
+        String userAgent = Servlets.getHeaderValue(request, "User-Agent");
+        if (Strings.contains(userAgent, "MicroMessenger")) {
+            return ActionChannelEnum.wechat;
+        }
+        String[] iosMobiles = {"iPod", "iPad", "iPhone", "Android", "SymbianOS", "Windows Phone"};
+        for (String s : iosMobiles) {
+            if (Strings.containsIgnoreCase(userAgent, s)) {
+                return ActionChannelEnum.wap;
+            }
+        }
+        return ActionChannelEnum.web;
+    }
 }

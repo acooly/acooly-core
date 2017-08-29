8d9337db0df24449c9a7426a0b6df0885f9ce42d
邱占波 <bohr.qiu@gmail.com>
2017-08-29 12:49:59 +0800

    refactor

--- acooly-components/acooly-component-dubbo/src/main/java/com/acooly/module/dubbo/mock/DubboMockBeanPostProcessor.java
+++ acooly-components/acooly-component-dubbo/src/main/java/com/acooly/module/dubbo/mock/DubboMockBeanPostProcessor.java
@@ -11,68 +11,79 @@ import org.springframework.util.ReflectionUtils;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author qiubo@yiji.com
- */
+/** @author qiubo@yiji.com */
 @Slf4j
 public class DubboMockBeanPostProcessor implements BeanPostProcessor {
-    private String annotationPackage;
-    private List<String> mockInterfaces;
+  private String annotationPackage;
+  private List<String> mockInterfaces;
 
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        return bean;
+  @Override
+  public Object postProcessBeforeInitialization(Object bean, String beanName)
+      throws BeansException {
+    return bean;
+  }
+
+  private boolean isMatchPackage(Object bean) {
+    return isMatchPackage(bean.getClass());
+  }
+
+  private boolean isMatchPackage(Class clazz) {
+    if (annotationPackage == null) {
+      return true;
     }
-    private boolean isMatchPackage(Object bean) {
-        return isMatchPackage(bean.getClass());
+    String beanClassName = clazz.getName();
+    return beanClassName.startsWith(annotationPackage);
+  }
+
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    if (mockInterfaces == null) {
+      return bean;
     }
-    private boolean isMatchPackage(Class clazz) {
-        if (annotationPackage == null ) {
-            return true;
-        }
-        String beanClassName = clazz.getName();
-        return beanClassName.startsWith(annotationPackage);
+    if (!isMatchPackage(bean)) {
+      return bean;
     }
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if(mockInterfaces==null){
-            return bean;
-        }
-        if (!isMatchPackage(bean)) {
-            return bean;
-        }
-        //处理代理bean，不能获取targetClass
-        ReflectionUtils.doWithFields(bean.getClass(), field -> {
-            Reference reference = field.getAnnotation(Reference.class);
-            if (reference != null) {
-                if(mockInterfaces.contains(field.getType().getName())){
-                    field.setAccessible(true);
-                    Map<String, ?> beansOfType = Apps.getApplicationContext().getBeansOfType(field.getType());
-                    Object mockService=null;
-                    for (Object o : beansOfType.values()) {
-                        if(o.getClass().getName().endsWith("Mock")){
-                            mockService=o;
-                        }
-                    }
-                    if(mockService==null){
-                        throw new AppConfigException("dubbo消费者:"+field.getType()+" mock实现类不存在,类名必须以Mock为后缀");
-                    }
-                    field.set(bean, mockService);
-                    log.info("[MOCK]dubbo @Reference {}.{} has bean mocked with {}",bean.getClass().getSimpleName(),field.getName(),mockService.getClass().getName());
+    //处理代理bean，不能获取targetClass
+    ReflectionUtils.doWithFields(
+        bean.getClass(),
+        field -> {
+          Reference reference = field.getAnnotation(Reference.class);
+          if (reference != null) {
+            if (mockInterfaces.contains(field.getType().getName())) {
+              field.setAccessible(true);
+              Map<String, ?> beansOfType =
+                  Apps.getApplicationContext().getBeansOfType(field.getType());
+              Object mockService = null;
+              for (Object o : beansOfType.values()) {
+                if (o.getClass().getName().endsWith("Mock")) {
+                  mockService = o;
                 }
+              }
+              if (mockService == null) {
+                throw new AppConfigException(
+                    "dubbo消费者:" + field.getType() + " mock实现类不存在,类名必须以Mock为后缀");
+              }
+              field.set(bean, mockService);
+              log.info(
+                  "[MOCK]dubbo @Reference {}.{} has bean mocked with {}",
+                  field.getDeclaringClass().getSimpleName(),
+                  field.getName(),
+                  mockService.getClass().getName());
             }
+          }
         });
-        return bean;
-    }
-    public void setAnnotationPackage(String annotationPackages) {
-        this.annotationPackage = annotationPackage;
-    }
+    return bean;
+  }
 
-    public List<String> getMockInterfaces() {
-        return mockInterfaces;
-    }
+  public void setAnnotationPackage(String annotationPackages) {
+    this.annotationPackage = annotationPackage;
+  }
 
-    public void setMockInterfaces(List<String> mockInterfaces) {
-        this.mockInterfaces = mockInterfaces;
-    }
+  public List<String> getMockInterfaces() {
+    return mockInterfaces;
+  }
+
+  public void setMockInterfaces(List<String> mockInterfaces) {
+    this.mockInterfaces = mockInterfaces;
+  }
 }

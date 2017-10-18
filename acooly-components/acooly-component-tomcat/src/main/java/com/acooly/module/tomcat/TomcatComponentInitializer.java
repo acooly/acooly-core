cd956f6a5adcb62eb6a7298e2cc2bc24cc781a5d
邱占波 <bohr.qiu@gmail.com>
2017-10-18 21:33:14 +0800

    refactor

--- acooly-components/acooly-component-tomcat/src/main/java/com/acooly/module/tomcat/TomcatComponentInitializer.java
+++ acooly-components/acooly-component-tomcat/src/main/java/com/acooly/module/tomcat/TomcatComponentInitializer.java
@@ -18,11 +18,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class TomcatComponentInitializer implements ComponentInitializer {
   @Override
   public void initialize(ConfigurableApplicationContext applicationContext) {
-    //enable org.apache.catalina.valves.RemoteIpValve for get real ip （ accesslog and request.getRemoteAddr() all will get real ip ）
-    System.setProperty("server.tomcat.remote_ip_header", "x-forwarded-for");
-    //enable https redirct to relative location
-    System.setProperty("server.tomcat.protocol_header", "x-forwarded-proto");
-    System.setProperty("server.tomcat.port-header", "x-forwarded-port");
+    setPropertyIfMissing("server.tomcat.remoteIpHeader", "x-forwarded-for");
+    setPropertyIfMissing("server.tomcat.protocolHeader", "x-forwarded-proto");
+    setPropertyIfMissing("server.tomcat.portHeader", "x-forwarded-port");
     if (applicationContext.getEnvironment().getProperty("acooly.tomcat.port") != null) {
       System.setProperty(
           Apps.HTTP_PORT, applicationContext.getEnvironment().getProperty("acooly.tomcat.port"));

e9700881fde099884e0d1c5e4ddad769d91e7009
张浦 <kuli@yiji.com>
2017-10-29 00:48:19 +0800

    为feedback的类型增加other选项

--- acooly-components/acooly-component-portlet/src/main/java/com/acooly/module/portlet/enums/FeedbackTypeEnum.java
+++ acooly-components/acooly-component-portlet/src/main/java/com/acooly/module/portlet/enums/FeedbackTypeEnum.java
@@ -1,4 +1,6 @@
-/** create by zhangpu date:2015年5月19日 */
+/**
+ * create by zhangpu date:2015年5月19日
+ */
 package com.acooly.module.portlet.enums;
 
 import com.acooly.core.utils.enums.Messageable;
@@ -10,81 +12,83 @@ import java.util.Map;
 
 /** @author zhangpu */
 public enum FeedbackTypeEnum implements Messageable {
-  suggest("suggest", "建议"),
+    suggest("suggest", "建议"),
 
-  complaint("complaint", "投诉");
+    complaint("complaint", "投诉"),
 
-  private final String code;
-  private final String message;
+    other("other", "其他");
 
-  FeedbackTypeEnum(String code, String message) {
-    this.code = code;
-    this.message = message;
-  }
+    private final String code;
+    private final String message;
 
-  public static Map<String, String> mapping() {
-    Map<String, String> map = Maps.newLinkedHashMap();
-    for (FeedbackTypeEnum type : values()) {
-      map.put(type.getCode(), type.getMessage());
+    FeedbackTypeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
     }
-    return map;
-  }
 
-  /**
-   * 通过枚举值码查找枚举值。
-   *
-   * @param code 查找枚举值的枚举值码。
-   * @return 枚举值码对应的枚举值。
-   * @throws IllegalArgumentException 如果 code 没有对应的 Status 。
-   */
-  public static FeedbackTypeEnum find(String code) {
-    for (FeedbackTypeEnum status : values()) {
-      if (status.getCode().equals(code)) {
-        return status;
-      }
+    public static Map<String, String> mapping() {
+        Map<String, String> map = Maps.newLinkedHashMap();
+        for (FeedbackTypeEnum type : values()) {
+            map.put(type.getCode(), type.getMessage());
+        }
+        return map;
     }
-    throw new IllegalArgumentException("FeedbackType not legal:" + code);
-  }
 
-  /**
-   * 获取全部枚举值。
-   *
-   * @return 全部枚举值。
-   */
-  public static List<FeedbackTypeEnum> getAll() {
-    List<FeedbackTypeEnum> list = new ArrayList<FeedbackTypeEnum>();
-    for (FeedbackTypeEnum status : values()) {
-      list.add(status);
+    /**
+     * 通过枚举值码查找枚举值。
+     *
+     * @param code 查找枚举值的枚举值码。
+     * @return 枚举值码对应的枚举值。
+     * @throws IllegalArgumentException 如果 code 没有对应的 Status 。
+     */
+    public static FeedbackTypeEnum find(String code) {
+        for (FeedbackTypeEnum status : values()) {
+            if (status.getCode().equals(code)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("FeedbackType not legal:" + code);
     }
-    return list;
-  }
 
-  /**
-   * 获取全部枚举值码。
-   *
-   * @return 全部枚举值码。
-   */
-  public static List<String> getAllCode() {
-    List<String> list = new ArrayList<String>();
-    for (FeedbackTypeEnum status : values()) {
-      list.add(status.code());
+    /**
+     * 获取全部枚举值。
+     *
+     * @return 全部枚举值。
+     */
+    public static List<FeedbackTypeEnum> getAll() {
+        List<FeedbackTypeEnum> list = new ArrayList<FeedbackTypeEnum>();
+        for (FeedbackTypeEnum status : values()) {
+            list.add(status);
+        }
+        return list;
     }
-    return list;
-  }
 
-  public String getCode() {
-    return code;
-  }
+    /**
+     * 获取全部枚举值码。
+     *
+     * @return 全部枚举值码。
+     */
+    public static List<String> getAllCode() {
+        List<String> list = new ArrayList<String>();
+        for (FeedbackTypeEnum status : values()) {
+            list.add(status.code());
+        }
+        return list;
+    }
 
-  public String getMessage() {
-    return message;
-  }
+    public String getCode() {
+        return code;
+    }
 
-  public String code() {
-    return code;
-  }
+    public String getMessage() {
+        return message;
+    }
 
-  public String message() {
-    return message;
-  }
+    public String code() {
+        return code;
+    }
+
+    public String message() {
+        return message;
+    }
 }

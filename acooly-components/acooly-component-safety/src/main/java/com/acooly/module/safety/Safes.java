150cdf65f604f856d56038fffb656c430a501e62
张浦 <kuli@yiji.com>
2017-11-14 13:19:39 +0800

    重构Safes工具类为不依赖spring内部执行序，存静态工具类。

--- acooly-components/acooly-component-safety/src/main/java/com/acooly/module/safety/Safes.java
+++ acooly-components/acooly-component-safety/src/main/java/com/acooly/module/safety/Safes.java
@@ -5,40 +5,35 @@
 
 /*
  * 修订记录:
- * zhangpu@acooly.cn 2017-10-08 23:07 创建
+ * zhangpu@acooly.cn 2017-11-14 12:38 创建
  */
 package com.acooly.module.safety;
 
+import com.acooly.core.common.boot.Apps;
 import com.acooly.module.safety.signature.SignTypeEnum;
 import com.acooly.module.safety.signature.Signer;
 import com.acooly.module.safety.signature.SignerFactory;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 
 /**
- * @author zhangpu 2017-10-08 23:07
+ * @author zhangpu 2017-11-14 12:38
  */
-@Component
 public class Safes {
 
-    @Resource(name = "safetySignerFactory")
-    private SignerFactory signerFactory;
-
-    private static Safes safes;
-
-    @PostConstruct
-    public void init() {
-        safes = this;
-    }
+    private static SignerFactory signerFactory;
 
     public static Signer getSigner(String signType) {
-        return safes.signerFactory.getSigner(signType);
+        if (signerFactory == null) {
+            synchronized (Safes.class) {
+                if (signerFactory == null) {
+                    signerFactory = Apps.getApplicationContext().getBean(SignerFactory.class);
+                }
+            }
+        }
+        return signerFactory.getSigner(signType);
     }
 
     public static Signer getSigner(SignTypeEnum signType) {
-        return safes.signerFactory.getSigner(signType.name());
+        return Safes.getSigner(signType.name());
     }
 
 }

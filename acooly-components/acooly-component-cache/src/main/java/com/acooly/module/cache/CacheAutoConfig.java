435f6c2e624f0c238adc89176e835b6dc13234f3
bohr.qiu <bohr.qiu@gmail.com>
2018-02-01 11:22:09 +0800

    修复依赖cache依赖错误

--- acooly-components/acooly-component-cache/src/main/java/com/acooly/module/cache/CacheAutoConfig.java
+++ acooly-components/acooly-component-cache/src/main/java/com/acooly/module/cache/CacheAutoConfig.java
@@ -11,8 +11,10 @@ package com.acooly.module.cache;
 
 import com.acooly.module.cache.declarative.DefaultCachingConfigurer;
 import com.acooly.module.cache.declarative.RedisInfoChecker;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -51,7 +53,11 @@ public class CacheAutoConfig {
       RedisTemplate redisTemplate, CacheProperties cacheProperties) {
     return new DefaultCachingConfigurer(redisTemplate, cacheProperties.getExpireTime());
   }
-
+  @Bean
+  @ConditionalOnMissingBean
+  public CacheManager cacheManager(CachingConfigurer cachingConfigurer){
+    return cachingConfigurer.cacheManager();
+  }
   //session使用kryo序列化器 ref:org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration.setDefaultRedisSerializer()
   //	@Bean("springSessionDefaultRedisSerializer")
   //由于实体对象中有懒加载集合，不能用kryo。。。

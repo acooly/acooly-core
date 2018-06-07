ea8889f8fd869bfc077ca70155723461517ed724
shuijing <shuijing@yiji.com>
2018-06-07 12:07:58 +0800

    去掉common包多余的

--- acooly-common/src/main/java/com/acooly/core/common/facade/DtoAble.java
+++
@@ -1,9 +0,0 @@
-package com.acooly.core.common.facade;
-
-/**
- * @author qiuboboy@qq.com
- * @date 2018-04-08 16:50
- */
-public interface DtoAble {
-    Object getDto();
-}
--- acooly-common/src/main/java/com/acooly/core/utils/conversion/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 提供类型转换功能的包。
- */
-package com.acooly.core.utils.conversion;

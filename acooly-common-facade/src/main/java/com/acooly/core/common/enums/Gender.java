bd431d181efc27ad3c65f54294118e9a8afdbd80
zhangpu <zhangpu@acooly.cn>
2018-07-31 18:38:11 +0800

    加入其他helper方法，兼容汉字性别.

--- acooly-common-facade/src/main/java/com/acooly/core/common/enums/Gender.java
+++ acooly-common-facade/src/main/java/com/acooly/core/common/enums/Gender.java
@@ -1,37 +1,101 @@
 package com.acooly.core.common.enums;
 
 import com.acooly.core.utils.enums.Messageable;
+import com.google.common.collect.Maps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author qiuboboy@qq.com
+ * @zhangpu 加入其他helper方法，兼容汉字性别 2018-07-31
  * @date 2018-07-31 13:38
  */
 public enum Gender implements Messageable {
-    MALE("M", "男"), FEMALE("F", "女"),UNKOWN("U","未知");
+    male("male", "男"),
+    female("female", "女");
+
     private final String code;
     private final String message;
 
-    Gender(String code, String message) {
+    private Gender(String code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    @Override
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
     public String code() {
-        return this.code;
+        return code;
     }
 
-    @Override
     public String message() {
-        return this.message;
+        return message;
     }
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Gender{");
-        sb.append("code='").append(code).append('\'');
-        sb.append(", message='").append(message).append('\'');
-        sb.append('}');
-        return sb.toString();
+    public static Map<String, String> mapping() {
+        Map<String, String> map = Maps.newLinkedHashMap();
+        for (Gender type : values()) {
+            map.put(type.getCode(), type.getMessage());
+        }
+        return map;
     }
+
+    /**
+     * 通过枚举值码查找枚举值。
+     *
+     * @param code 查找枚举值的枚举值码。
+     * @return 枚举值码对应的枚举值。
+     * @throws IllegalArgumentException 如果 code 没有对应的 Status 。
+     */
+    public static Gender find(String code) {
+        for (Gender status : values()) {
+            if (status.getCode().equals(code)) {
+                return status;
+            }
+        }
+
+        if (Gender.male.message.equals(code)) {
+            return Gender.male;
+        }
+        if (Gender.female.message.equals(code)) {
+            return Gender.female;
+        }
+
+        throw null;
+    }
+
+    /**
+     * 获取全部枚举值。
+     *
+     * @return 全部枚举值。
+     */
+    public static List<Gender> getAll() {
+        List<Gender> list = new ArrayList<Gender>();
+        for (Gender status : values()) {
+            list.add(status);
+        }
+        return list;
+    }
+
+    /**
+     * 获取全部枚举值码。
+     *
+     * @return 全部枚举值码。
+     */
+    public static List<String> getAllCode() {
+        List<String> list = new ArrayList<String>();
+        for (Gender status : values()) {
+            list.add(status.code());
+        }
+        return list;
+    }
+
 }

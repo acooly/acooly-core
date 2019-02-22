d1712a83b4a8a17be566564bbe17e1cf853253d0
zhangpu <zhangpu@acooly.cn>
2019-02-22 16:53:47 +0800

    调整ofile组件的后台管理功能图标为awesome

--- acooly-components/acooly-component-ofile/src/main/resources/META-INF/database/ofile/mysql/ofile_urls.sql
+++ acooly-components/acooly-component-ofile/src/main/resources/META-INF/database/ofile/mysql/ofile_urls.sql
@@ -1,6 +1,2 @@
-INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`) VALUES ('20170219', NULL, '文件管理', 'MENU', '0', '2017-02-19 18:32:05', '', '1', 'icons-resource-caozuorizhichaxun', NULL);
-INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`) VALUES ('201702191', '20170219', '文件管理', 'URL', '0', '2017-02-19 18:32:05', '/manage/module/ofile/onlineFile/index.html', '1', 'icons-resource-iconfontcolor28', NULL);
-
-
-INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '20170219');
+INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`) VALUES ('201702191', '2019022201', '文件管理', 'URL', '0', '2017-02-19 18:32:05', '/manage/module/ofile/onlineFile/index.html', '1', 'fa-circle-o', NULL);
 INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '201702191');

5372d8e1344adc01caa20fc76ead1f3cca38d604
zhangpu <zhangpu@acooly.cn>
2019-10-08 18:49:01 +0800

    新增：buildQueryString方法，优化getParameters方法

--- acooly-common/src/main/java/com/acooly/core/utils/Servlets.java
+++ acooly-common/src/main/java/com/acooly/core/utils/Servlets.java
@@ -242,7 +242,27 @@ public class Servlets {
      * <p>返回的结果的Parameter名已去除前缀.
      */
     public static Map<String, Object> getParametersStartingWith(ServletRequest request, String prefix) {
-        return Maps.transformValues(getParameters(request, prefix, false), v -> String.valueOf(v));
+        Validate.notNull(request, "Request must not be null");
+        Enumeration paramNames = request.getParameterNames();
+        Map<String, Object> params = new TreeMap<String, Object>();
+        if (prefix == null) {
+            prefix = "";
+        }
+        while (paramNames != null && paramNames.hasMoreElements()) {
+            String paramName = (String) paramNames.nextElement();
+            if ("".equals(prefix) || paramName.startsWith(prefix)) {
+                String unprefixed = paramName.substring(prefix.length());
+                String[] values = request.getParameterValues(paramName);
+                if (values == null || values.length == 0) {
+                    // Do nothing, no values found at all.
+                } else if (values.length > 1) {
+                    params.put(unprefixed, values);
+                } else {
+                    params.put(unprefixed, values[0]);
+                }
+            }
+        }
+        return params;
     }
 
     public static Map<String, String> getParameters(ServletRequest request, String prefix, boolean includeEmpty) {

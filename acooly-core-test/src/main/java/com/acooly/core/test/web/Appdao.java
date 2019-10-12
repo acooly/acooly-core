050a50e3431faa1e157a34f2041b197f866d665f
zhouxi <fancy9044@gmail.com>
2019-10-12 15:45:36 +0800

    修复分页在有事物的情况下，获取总数不对问题！

---
+++ acooly-core-test/src/main/java/com/acooly/core/test/web/Appdao.java
@@ -0,0 +1,14 @@
+package com.acooly.core.test.web;
+
+import com.acooly.core.test.domain.App;
+import com.acooly.module.mybatis.EntityMybatisDao;
+
+/**
+ * Zhouxi O_o
+ *
+ * @author xi
+ * @description
+ */
+public interface Appdao extends EntityMybatisDao<App> {
+
+}

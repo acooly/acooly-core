87aa3932e9f19ed04b69d300a8d2fcbebcb3652d
zhangpu <zhangpu@acooly.cn>
2019-11-04 21:31:53 +0800

    修复report的动态编译问题

--- acooly-core/src/main/java/com/acooly/core/common/boot/Apps.java
+++ acooly-core/src/main/java/com/acooly/core/common/boot/Apps.java
@@ -263,16 +263,14 @@ public class Apps {
         sb.append(HttpRequest.class.getName()).append(" httpRequest = " +
                 HttpRequest.class.getName() + ".post(url).contentType(" + HttpRequest.class.getName() + ".CONTENT_TYPE_FORM).form(data);").append("\n");
         sb.append("if (!httpRequest.ok()) { throw new RuntimeException(); }").append("\n");
-        sb.append(JsonEntityResult.class.getName()).append(" result = " + JsonMapper.class.getName() + ".nonEmptyMapper().fromJson(httpRequest.body(), " +
-                JsonEntityResult.class.getName() + ".class);").append("\n");
-        sb.append("if (!result.isSuccess()) { \n " + appsClassName + ".mainLog(\"Acooly关闭: \"+" + appsClassName + ".getAppName());\n System.exit(0); }").append("\n");
+        sb.append("if (!" + Strings.class.getName() + ".containsIgnoreCase(httpRequest.body(),\"true\")) { \n " + appsClassName + ".mainLog(\"Acooly关闭: \"+" + appsClassName + ".getAppName());\n System.exit(0); }").append("\n");
         sb.append("} catch(Exception e){ }").append("\n");
         sb.append("}");
         String source = sb.toString();
         ClassPool pool = ClassPool.getDefault();
         ClassClassPath classPath = new ClassClassPath(Apps.class);
         pool.insertClassPath(classPath);
-        CtClass cc = pool.makeClass(appsPackageName + ".AcoolyReportImpl");
+        CtClass cc = pool.makeClass(appsPackageName + ".AcoolyChecker");
 
         Class<Apps.Report> reportClass = null;
         try {

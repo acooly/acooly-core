274d2f8e4585d6722dfdc615ee3262cb20055b46
zhouxi <fancy9044@gmail.com>
2019-12-02 17:27:52 +0800

    禁止自动装配

--- acooly-core/src/main/java/com/acooly/core/common/boot/listener/AcoolyApplicationRunListener.java
+++ acooly-core/src/main/java/com/acooly/core/common/boot/listener/AcoolyApplicationRunListener.java
@@ -211,10 +211,15 @@ public class AcoolyApplicationRunListener implements SpringApplicationRunListene
 
         Assert.hasLength(sysName, "系统名不能为空");
         System.setProperty(Apps.APP_NAME, sysName);
-        System.setProperty("spring.cloud.bootstrap.enabled",
-                Boolean.toString(bootApp.enableSpringCloud()));
-        System.setProperty("spring.cloud.service-registry.auto-registration.enabled",
-                Boolean.toString(bootApp.enableSpringCloud()));
+        // spring cloud 版本太乱，防止版本对不上造成问题，提前引入依赖，并禁用
+        if (!bootApp.enableSpringCloud()) {
+            System.setProperty("spring.cloud.bootstrap.enabled", "false");
+            System.setProperty("spring.autoconfigure.exclude",
+                    "org.springframework.cloud.alibaba.nacos.NacosDiscoveryAutoConfiguration,"
+                            + "org.springframework.cloud.alibaba.nacos.ribbon.RibbonNacosAutoConfiguration,"
+                            + "org.springframework.cloud.alibaba.nacos.endpoint.NacosDiscoveryEndpointAutoConfiguration,"
+                            + "org.springframework.cloud.alibaba.nacos.discovery.NacosDiscoveryClientAutoConfiguration");
+        }
         // ref ContextIdApplicationContextInitializer
         System.setProperty("spring.application.name", sysName);
         //set servlet container display name

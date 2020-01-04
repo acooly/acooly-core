281ac4f727dcf844e646cc2b5bdf900f02cc94dd
zhangpu <zhangpu@prosysoft.com>
2020-01-05 00:51:37 +0800

    升级Freemarker的工具方法，替换过期Api和优化工具方法结构

--- acooly-common/src/main/java/com/acooly/core/utils/FreeMarkers.java
+++ acooly-common/src/main/java/com/acooly/core/utils/FreeMarkers.java
@@ -8,9 +8,13 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.net.URL;
 import java.util.Map;
 
+/**
+ * freemarker 简单工具
+ *
+ * @author zhangpu
+ */
 public class FreeMarkers {
 
     /**
@@ -19,7 +23,8 @@ public class FreeMarkers {
     public static String rendereString(String templateString, Map<String, ?> model) {
         try {
             StringWriter result = new StringWriter();
-            Template t = new Template("name", new StringReader(templateString), new Configuration());
+            Template t = new Template("name", new StringReader(templateString),
+                    new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS));
             t.process(model, result);
             return result.toString();
         } catch (Exception e) {
@@ -31,6 +36,14 @@ public class FreeMarkers {
      * 渲染Template文件.
      */
     public static String renderTemplate(Template template, Object model) {
+        return doRenderTemplate(template, model);
+    }
+
+    public static String renderTemplate(Template template, Map<String, ?> model) {
+        return doRenderTemplate(template, model);
+    }
+
+    public static String doRenderTemplate(Template template, Object model) {
         try {
             StringWriter result = new StringWriter();
             template.process(model, result);
@@ -40,21 +53,12 @@ public class FreeMarkers {
         }
     }
 
-    public static String renderTemplate(Template template, Map<String, ?> model) {
-        try {
-            StringWriter result = new StringWriter();
-            template.process(model, result);
-            return result.toString();
-        } catch (Exception e) {
-            throw Exceptions.unchecked(e);
-        }
-    }
 
     /**
      * 创建默认配置，设定模板目录.
      */
     public static Configuration buildConfiguration(String directory) throws IOException {
-        Configuration cfg = new Configuration();
+        Configuration cfg = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
 
         Resource path = new DefaultResourceLoader().getResource(directory);
         cfg.setDirectoryForTemplateLoading(path.getFile());

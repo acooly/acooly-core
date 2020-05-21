3fd055039ba94826c07593befb3787bb75c065ed
zhangpu <zhangpu@prosysoft.com>
2020-05-22 00:46:43 +0800

    增加smsend组件的版本管理

--- acooly-core-test/src/main/java/com/acooly/core/test/dubbo/DefaultTestService.java
+++ acooly-core-test/src/main/java/com/acooly/core/test/dubbo/DefaultTestService.java
@@ -1,25 +1,25 @@
-//package com.acooly.core.test.dubbo;
-//
-//import com.alibaba.dubbo.config.annotation.Service;
-//import com.alibaba.dubbo.rpc.RpcContext;
-//
-///**
-// * Zhouxi O_o
-// *
-// * @author xi
-// * @description
-// */
-//@Service(version = "1.0.0")
-//public class DefaultTestService implements TestService {
-//
-//    @Override
-//    public String sayHello( String str ) {
-//        RpcContext rpcContext = RpcContext.getContext();
-//        return String.format("Service [name :%s , port : %d] %s(\"%s\") : Hello,%s",
-//                "test",
-//                rpcContext.getLocalPort(),
-//                rpcContext.getMethodName(),
-//                str,
-//                str);
-//    }
-//}
+package com.acooly.core.test.dubbo;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
+
+/**
+ * Zhouxi O_o
+ *
+ * @author xi
+ * @description
+ */
+@Service(version = "1.0.0")
+public class DefaultTestService implements TestService {
+
+    @Override
+    public String sayHello(String str) {
+        RpcContext rpcContext = RpcContext.getContext();
+        return String.format("Service [name :%s , port : %d] %s(\"%s\") : Hello,%s",
+                "test",
+                rpcContext.getLocalPort(),
+                rpcContext.getMethodName(),
+                str,
+                str);
+    }
+}

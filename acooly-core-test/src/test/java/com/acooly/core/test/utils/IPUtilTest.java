6818e018383b9f8af2ff89d5ad443af358ab103d
zhangpu <zhangpu@prosysoft.com>
2020-11-09 00:42:50 +0800

    设置web组件的shiro-framework-tag依赖忽悠shiro的依赖，以保证项目中shiro版本一致。

---
+++ acooly-core-test/src/test/java/com/acooly/core/test/utils/IPUtilTest.java
@@ -0,0 +1,27 @@
+/**
+ * acooly-core-parent
+ * <p>
+ * Copyright 2014 Acooly.cn, Inc. All rights reserved.
+ *
+ * @author zhangpu
+ * @date 2020-10-28 00:33
+ */
+package com.acooly.core.test.utils;
+
+import com.acooly.core.utils.system.IPUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ *
+ * @author zhangpu
+ * @date 2020-10-28 00:33
+ */
+@Slf4j
+public class IPUtilTest {
+
+    @Test
+    public void testOne(){
+
+    }
+}

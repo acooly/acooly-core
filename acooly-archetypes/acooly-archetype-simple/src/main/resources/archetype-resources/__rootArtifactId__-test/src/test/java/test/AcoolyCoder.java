57e7a6d726f28ef07778d74518d3884889eddeef
zhangpu <zhangpu@prosysoft.com>
2021-05-06 15:59:13 +0800

    更新acooly-archetype-simple的AcoolyCoder代码

--- acooly-archetypes/acooly-archetype-simple/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/AcoolyCoder.java
+++ acooly-archetypes/acooly-archetype-simple/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/AcoolyCoder.java
@@ -1,12 +1,14 @@
 #set($symbol_pound='#')
-#set($symbol_dollar='$')
-#set($symbol_escape='\' )
+        #set($symbol_dollar='$')
+        #set($symbol_escape='\' )
 
-package ${package}.test;
+        package ${package}.test;
+
 import com.acooly.coder.Generator;
 import com.acooly.coder.config.GenerateConfig;
 import com.acooly.coder.generate.CodeGenerateService;
-
+import com.acooly.coder.module.GenerateModule;
+import com.google.common.collect.Sets;
 
 /**
  * 代码生成工具
@@ -16,15 +18,15 @@ import com.acooly.coder.generate.CodeGenerateService;
  */
 public class AcoolyCoder {
     // 生成代码的目标模块
-    static String MODULE_NAME = "${rootArtifactId}-core";
+    static String MODULE_NAME = "${parentArtifactId}-platform/${parentArtifactId}-platform-core";
     // 生成代码的根包
-    static String ROOT_PACKAGE = "${package}";
+    static String ROOT_PACKAGE = "${package}.platform.core";
     // 生成代码的管理视图相对路径
-    static String MANAGE_VIEW_PATH = "/manage/";
+    static String MANAGE_VIEW_PATH = "/manage/platform/";
     // 配置表名转换为实体名时，需要忽略的表前缀。例如配置：p_ 则表示p_customer(表名) -> Customer(实体类名)
-    static String TABLE_IGNOR_PREFIX = "a_acooly_";
+    static String TABLE_IGNOR_PREFIX = "acooly_";
     // 生成代码的表
-    static String[] TABLES = {"a_acooly_project"};
+    static String[] TABLES = {"acooly_coder_customer"};
 
     /**
      * 代码方式配置关键参数
@@ -40,7 +42,12 @@ public class AcoolyCoder {
         config.setManagePath(MANAGE_VIEW_PATH);
         config.setTableToEntityIgnorPrefix(TABLE_IGNOR_PREFIX);
         config.setRootPackage(ROOT_PACKAGE);
-        service.generateTable(config, TABLES);
+        Set<GenerateModule> modules = Sets.newLinkedHashSet(GenerateModule.Manage
+//                , GenerateModule.Facade
+//                , GenerateModule.OpenApi
+        );
+        config.setGeneratorModules(modules);
+        service.generateTable(TABLES);
     }
 
     public static String getProjectPath() {

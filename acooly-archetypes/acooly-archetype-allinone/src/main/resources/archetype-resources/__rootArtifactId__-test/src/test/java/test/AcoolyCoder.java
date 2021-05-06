b90a17788354f9df97b2071d15185334ca1be5bc
zhangpu <zhangpu@prosysoft.com>
2021-05-06 13:29:55 +0800

    更新AcoolyCoder工具方法的入口执行方法和添加对facade和OpenApi模块的支持

--- acooly-archetypes/acooly-archetype-allinone/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/AcoolyCoder.java
+++ acooly-archetypes/acooly-archetype-allinone/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/AcoolyCoder.java
@@ -1,13 +1,14 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
+#set($symbol_pound='#')
+        #set($symbol_dollar='$')
+        #set($symbol_escape='\' )
 
-package ${package}.test;
+        package ${package}.test;
 
 import com.acooly.coder.Generator;
 import com.acooly.coder.config.GenerateConfig;
 import com.acooly.coder.generate.CodeGenerateService;
-
+import com.acooly.coder.module.GenerateModule;
+import com.google.common.collect.Sets;
 
 /**
  * 代码生成工具
@@ -23,7 +24,7 @@ public class AcoolyCoder {
     // 生成代码的管理视图相对路径
     static String MANAGE_VIEW_PATH = "/manage/platform/";
     // 配置表名转换为实体名时，需要忽略的表前缀。例如配置：p_ 则表示p_customer(表名) -> Customer(实体类名)
-    static String TABLE_IGNOR_PREFIX = "acooly_coder_";
+    static String TABLE_IGNOR_PREFIX = "acooly_";
     // 生成代码的表
     static String[] TABLES = {"acooly_coder_customer"};
 
@@ -41,7 +42,12 @@ public class AcoolyCoder {
         config.setManagePath(MANAGE_VIEW_PATH);
         config.setTableToEntityIgnorPrefix(TABLE_IGNOR_PREFIX);
         config.setRootPackage(ROOT_PACKAGE);
-        service.generateTable(config, TABLES);
+        Set<GenerateModule> modules = Sets.newLinkedHashSet(GenerateModule.Manage
+//                , GenerateModule.Facade
+//                , GenerateModule.OpenApi
+        );
+        config.setGeneratorModules(modules);
+        service.generateTable(TABLES);
     }
 
     public static String getProjectPath() {

094df80d39dba33b5943a06e4d3896c228c6d127
zhangpu <zhangpu@prosysoft.com>
2021-08-22 19:25:34 +0800

    优化：filterchain组件的核心Context对象不再集成HashMap，调整为内部持有HashMap实例变量来实现内部动态变量，并提供了常规的put和get方法兼容。主要为解决：DEBUG时可以清楚的看到Context子类的实例变量值（Map的子类不会正常显示实例变量）

--- acooly-components/acooly-component-filterchain/src/main/java/com/acooly/module/filterchain/Context.java
+++ acooly-components/acooly-component-filterchain/src/main/java/com/acooly/module/filterchain/Context.java
@@ -9,22 +9,38 @@
  */
 package com.acooly.module.filterchain;
 
+import com.google.common.collect.Maps;
+
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * 上下文对象
  *
  * @author qiubo@yiji.com
  */
-public abstract class Context extends HashMap<String, Object> implements Serializable {
+public abstract class Context implements Serializable {
     public transient Iterator<Filter<? extends Context>> iterator;
+
+    protected Map<String, Object> params = Maps.newHashMap();
 
     /**
      * 设置此对象可以重入filterChain
      */
     public void reentry() {
         this.iterator = null;
+    }
+
+    public Object put(String key, Object value) {
+        return this.params.put(key, value);
+    }
+
+    public void putAll(Map<? extends String, ?> m) {
+        this.params.putAll(m);
+    }
+
+    public Object get(Object key) {
+        return this.params.get(key);
     }
 }

bebaed6bd497358e90496878b3a3c00a9ee641b3
zhangpu <zhangpu@prosysoft.com>
2021-10-04 22:45:26 +0800

    增加组件acooly-component-data-ip的版本管理

--- acooly-core-test/src/test/java/com/acooly/core/test/utils/IPUtilTest.java
+++ acooly-core-test/src/test/java/com/acooly/core/test/utils/IPUtilTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.util.Locale;
 
 /**
  * @author zhangpu
@@ -29,6 +30,19 @@ public class IPUtilTest {
         // 增加IPUtil工具方法isInRange,判断IP是否在指定网段内（子网）。例如：`IPUtil.isInRange("192.168.1.2", "192.168.0.0/24")`
         log.info("in RANGE {}", IPUtil.isInRange("192.168.1.127", "192.168.1.64/26"));
         System.out.println(IPUtil.isInRange("125.85.30.118", "125.85.0.0/19"));
+
+        log.info("{}", Locale.CHINA.getCountry());
+
+        Locale[] locales = Locale.getAvailableLocales();
+        for (Locale locale : locales) {
+            log.info("中文名称：{} , 英文名称：{}, 国家简称：{}, 语言简称：{}"
+                    ,locale.getDisplayCountry()
+                    ,locale.getDisplayCountry(Locale.ENGLISH)
+                    ,locale.getCountry()
+                    ,locale.toString());
+
+        }
+
     }
 
     @Test
@@ -38,13 +52,18 @@ public class IPUtilTest {
     }
 
     public static boolean isChineseIp(String ip) {
-        String chineseIps = HttpRequest.get("http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone").body();
+        String chineseIps = HttpRequest.get("http://www.ipdeny.com/ipblocks/data/countries/cn.zone").body();
         try (BufferedReader br = new BufferedReader(new StringReader(chineseIps))) {
-            if (IPUtil.isInRange(ip, Strings.trimToEmpty(br.readLine()))) {
-                return true;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                if (IPUtil.isInRange(ip, Strings.trimToEmpty(line))) {
+                    log.info("ip range match {} -> {}", line, ip);
+                    return true;
+                }
             }
+
         } catch (Exception e) {
-            // ig
+            e.printStackTrace();
         }
         return false;
     }

0eedb125366e9a95bac7a86e5643d726c8dcd2ca
zhangpu <zhangpu@prosysoft.com>
2022-09-12 16:55:26 +0800

    Excel文件导出增强

--- acooly-common/src/main/java/com/acooly/core/utils/ie/Exports.java
+++ acooly-common/src/main/java/com/acooly/core/utils/ie/Exports.java
@@ -8,18 +8,31 @@
  */
 package com.acooly.core.utils.ie;
 
-import com.acooly.core.utils.Reflections;
-import com.acooly.core.utils.Strings;
+import com.acooly.core.common.exception.BusinessException;
+import com.acooly.core.utils.*;
+import com.acooly.core.utils.enums.Messageable;
 import com.acooly.core.utils.ie.anno.ExportColumn;
 import com.acooly.core.utils.ie.anno.ExportModel;
+import com.acooly.core.utils.io.Streams;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.springframework.core.OrderComparator;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
 
+import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -31,6 +44,218 @@ import java.util.Map;
  */
 @Slf4j
 public class Exports {
+
+    public static ConversionService conversionService = new DefaultConversionService();
+
+
+    public static void exportExcel(List<?> list, String filePath) {
+        try {
+            OutputStream outputStream = Files.newOutputStream(Paths.get(filePath));
+            exportExcel(list,outputStream);
+        } catch (BusinessException be) {
+            throw be;
+        } catch (Exception e) {
+            throw new BusinessException();
+        }
+
+    }
+
+    /**
+     * 导出Excel
+     * @param list
+     * @param out
+     */
+    public static void exportExcel(List<?> list, OutputStream out) {
+        Asserts.notEmpty(list);
+        Object first = Collections3.getFirst(list);
+        ExportModelMeta exportModelMeta = parse(first);
+        doExportExcel(exportModelMeta, list, out);
+    }
+
+    protected static void doExportExcel(ExportModelMeta exportModelMeta, List<?> list, OutputStream out) {
+        SXSSFWorkbook workbook = null;
+        try {
+            List<String> headerNames = exportModelMeta.getHeaders();
+            workbook = new SXSSFWorkbook();
+            workbook.setCompressTempFiles(true);
+            Sheet sheet = workbook.createSheet();
+            int rowNum = 0;
+            // header
+            Row row = sheet.createRow(rowNum);
+            // header高度
+            if (exportModelMeta != null && exportModelMeta.getHeaderStyleMeta() != null
+                    && exportModelMeta.getHeaderStyleMeta().getRowHeight() != -1) {
+                row.setHeight(exportModelMeta.getHeaderStyleMeta().getRowHeight());
+            }
+            if (headerNames != null) {
+                // 构建通用CellStyle，并缓存到线程变量中
+                commonExcelCellStyle(sheet, exportModelMeta);
+                // 构建Header的CellStyle
+                CellStyle headerStyle = excelHeaderCellStyle(sheet, exportModelMeta);
+                for (int cellnum = 0; cellnum < headerNames.size(); cellnum++) {
+                    row.createCell(cellnum).setCellValue(headerNames.get(cellnum));
+                    if (headerStyle != null) {
+                        row.getCell(cellnum).setCellStyle(headerStyle);
+                    }
+                    updateExcelColumnWidth(cellnum, headerNames.get(cellnum), sheet);
+                }
+                rowNum++;
+            }
+            // 写数据
+            doExportExcelPage(list, rowNum, sheet, exportModelMeta);
+            workbook.write(out);
+            out.flush();
+        } catch (Exception e) {
+            log.warn("do export excel failure -> " + e.getMessage(), e);
+            throw new BusinessException("FILE_EXPORT_EXCEL_FAIL", "EXCEL文件输出失败", e.getMessage());
+        } finally {
+            Streams.closeQuietly(out);
+            Streams.closeQuietly(workbook);
+            workbook.dispose();
+        }
+
+    }
+
+    protected static int doExportExcelPage(List<?> list, int startRow, Sheet sheet, ExportModelMeta exportModelMeta) throws Exception {
+        int rowNum = startRow;
+        Object value;
+        Row row;
+        Cell cell;
+        CellStyle cellStyle = commonExcelCellStyle(sheet, exportModelMeta);
+        for (Object entity : list) {
+            List data = parseRow(exportModelMeta, entity);
+            row = sheet.createRow(rowNum);
+            for (int cellNum = 0; cellNum < data.size(); cellNum++) {
+                value = data.get(cellNum);
+                cell = row.createCell(cellNum);
+                if (cellStyle != null) {
+                    cell.setCellStyle(cellStyle);
+                }
+                // 空值直接返回
+                if (value == null) {
+                    cell.setCellType(CellType.BLANK);
+                    continue;
+                }
+
+                // Money转数字
+                if (value instanceof Money) {
+                    value = ((Money) value).getAmount();
+                }
+
+                // 枚举转字符串
+                if (value instanceof Messageable) {
+                    String temp = ((Messageable) value).message();
+                    if (exportModelMeta != null && exportModelMeta.getExportItem(cellNum) != null) {
+                        ExportColumnMeta item = exportModelMeta.getExportItem(cellNum);
+                        if (!item.isShowMapping()) {
+                            temp = ((Messageable) value).code();
+                        }
+                    }
+                    value = temp;
+                }
+
+                // 日期转字符串
+                if (value instanceof Date) {
+                    // 默认日期时间格式化foramt
+                    String foramt = Dates.CHINESE_DATETIME_FORMAT_LINE;
+                    if (Dates.isDate((Date) value)) {
+                        foramt = Dates.CHINESE_DATE_FORMAT_SLASH;
+                    }
+                    // 如果@anno设置了format，则按设置的格式进行转换
+                    if (exportModelMeta != null && exportModelMeta.getExportItem(cellNum) != null) {
+                        ExportColumnMeta item = exportModelMeta.getExportItem(cellNum);
+                        if (Strings.isNotBlank(item.getFormat())) {
+                            foramt = item.getFormat();
+                        }
+                    }
+                    value = Dates.format((Date) value, foramt);
+                }
+
+                // 数字或字符串处理
+                if (value instanceof Number) {
+                    cell.setCellType(CellType.NUMERIC);
+                    cell.setCellValue(conversionService.convert(value, Double.class));
+                } else {
+                    if (!(value instanceof String)) {
+                        value = conversionService.convert(value, String.class);
+                    }
+                    cell.setCellType(CellType.STRING);
+                    cell.setCellValue((String) value);
+                    updateExcelColumnWidth(cellNum, (String) value, sheet);
+                }
+
+                // 如果@anno设置了宽度，则覆盖设置
+                if (exportModelMeta != null && exportModelMeta.getExportItem(cellNum) != null) {
+                    ExportColumnMeta item = exportModelMeta.getExportItem(cellNum);
+                    if (item.getWidth() != -1) {
+                        sheet.setColumnWidth(cellNum, item.getWidth());
+                    }
+                }
+            }
+            rowNum = rowNum + 1;
+        }
+        return rowNum;
+    }
+
+
+    private static CellStyle commonExcelCellStyle(Sheet sheet, ExportModelMeta exportModelMeta) {
+        if (exportModelMeta.getStyle() == null) {
+            CellStyle cellStyle = sheet.getWorkbook().createCellStyle();
+            cellStyle.setWrapText(true);
+            if (exportModelMeta.isBorder()) {
+                cellStyle.setBorderTop(BorderStyle.THIN);
+                cellStyle.setBorderBottom(BorderStyle.THIN);
+                cellStyle.setBorderLeft(BorderStyle.THIN);
+                cellStyle.setBorderRight(BorderStyle.THIN);
+            }
+            exportModelMeta.setStyle(cellStyle);
+        }
+        return (CellStyle) exportModelMeta.getStyle();
+    }
+
+    private static CellStyle excelHeaderCellStyle(Sheet sheet, ExportModelMeta exportModelMeta) {
+        XSSFCellStyle cellStyle = (XSSFCellStyle) sheet.getWorkbook().createCellStyle();
+        cellStyle.setWrapText(true);
+        if (exportModelMeta.isBorder()) {
+            cellStyle.setBorderTop(BorderStyle.THIN);
+            cellStyle.setBorderBottom(BorderStyle.THIN);
+            cellStyle.setBorderLeft(BorderStyle.THIN);
+            cellStyle.setBorderRight(BorderStyle.THIN);
+        }
+
+        ExportStyleMeta headerStyleMeta = exportModelMeta.getHeaderStyleMeta();
+        if (headerStyleMeta != null) {
+            // 按需设置字体
+            if (headerStyleMeta.requireFont()) {
+                XSSFFont font = (XSSFFont) sheet.getWorkbook().createFont();
+                if (Strings.isNotBlank(headerStyleMeta.getFontName())) {
+                    font.setFontName(headerStyleMeta.getFontName());
+                }
+                if (headerStyleMeta.getFontSize() != -1) {
+                    font.setFontHeightInPoints(headerStyleMeta.getFontSize());
+                }
+                font.setBold(headerStyleMeta.isFontBold());
+                cellStyle.setFont(font);
+            }
+            // 按需设置背景填充色
+            if (Strings.isNotBlank(headerStyleMeta.getBackgroundColor())) {
+                cellStyle.setFillForegroundColor(new XSSFColor(java.awt.Color.decode(headerStyleMeta.getBackgroundColor())));
+                cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            }
+        }
+        return cellStyle;
+    }
+
+    private static void updateExcelColumnWidth(int colNum, String value, Sheet sheet) throws Exception {
+        int cellColumnWidth = (value.getBytes("UTF-8").length + 1) * 256;
+        if (cellColumnWidth > sheet.getColumnWidth(colNum)) {
+            if (cellColumnWidth <= 255 * 256) {
+                sheet.setColumnWidth(colNum, cellColumnWidth);
+            } else {
+                sheet.setColumnWidth(colNum, 100 * 255);
+            }
+        }
+    }
 
     /**
      * 解析导出元数据
@@ -123,14 +348,18 @@ public class Exports {
     }
 
     public static ExportModelMeta parse(ExportModelMeta result, Object object) {
+        result.setRow(parseRow(result, object));
+        return result;
+    }
+
+    public static List<Object> parseRow(ExportModelMeta result, Object object) {
         List<Object> row = Lists.newArrayList();
         for (ExportColumnMeta item : result.getItems()) {
             Object cell = Reflections.getFieldValue(object, item.getName());
             item.setValue(cell);
             row.add(cell);
         }
-        result.setRow(row);
-        return result;
+        return row;
     }
 
 
